4b68e43fa97031b52fc64e9185a8353ba96acf1b
Иван Иванов <gr422_iviev@lab-402-4.tomtit-tomsk.ru>
2024-11-06 13:42:25 +0700

    add preview for books

--- catalog/models.py
+++ catalog/models.py
@@ -17,6 +17,7 @@ class Book(models.Model):
     summary = models.TextField(max_length=1000, help_text="Enter a brief description of the book")
     isbn = models.CharField('ISBN',max_length=13, help_text='13 Character <a href="https://www.isbn-international.org/content/what-isbn">ISBN number</a>')
     genre = models.ManyToManyField(Genre, help_text="Select a genre for this book")
+    preview = models.ImageField(upload_to='catalog/books')
 
     def __str__(self):
         return self.title
--- locallibrary/urls.py
+++ locallibrary/urls.py
@@ -1,6 +1,5 @@
 from django.contrib import admin
 from django.urls import path,include
-from django.utils.translation.trans_real import catalog
 from django.views.generic import RedirectView
 from django.conf import settings
 from django.conf.urls.static import static
@@ -22,4 +21,7 @@ urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 urlpatterns += [
     path('accounts/', include('django.contrib.auth.urls')),
 ]
+
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
